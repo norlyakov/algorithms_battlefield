f7b96a023337beef6261a535d4cbb71642a25521
Nikita Orlyakov <norlyakov@asdco.ru>
2020-05-19 22:27:17 +0300

    Climbing Stairs.

---
+++ dynamic_programming/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""
+:Authors: norlyakov
+:Date: 19.05.2020
+"""
---
+++ dynamic_programming/climbing_stairs.py
@@ -0,0 +1,54 @@
+"""
+Climbing Stairs
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+"""
+
+
+def climbStairs(n: int) -> int:
+    prev1 = 1
+    prev2 = 0
+    res = 0
+    for i in range(n):
+        res = prev1 + prev2
+        prev2 = prev1
+        prev1 = res
+    return res
+
+
+def main():
+    n = 0
+    assert climbStairs(n) == 0
+
+    n = 1
+    assert climbStairs(n) == 1
+
+    n = 2
+    assert climbStairs(n) == 2
+
+    n = 6
+    assert climbStairs(n) == 13
+
+
+if __name__ == '__main__':
+    main()
